0f00b438b4af9274001b291a7142a342a9706d3f
Arijit-Kumar-Pandab <arijitkuamrpandab@gmail.com>
2021-08-02 09:34:14 +0530

    Checking Remote

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -58,6 +59,8 @@ class SleepTrackerFragment : Fragment() {
          * Initializing application, this will give us the context.
          */
         val application = requireNotNull(this.activity).application
+
+        Toast.makeText(context,"Check",Toast.LENGTH_SHORT).show()
 
         /**
          * Getting reference to database operations.
